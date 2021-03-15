bee96fbb9a2342ba888fb16c868f889c5550b04d
jconeff <jessicaconeff@gmail.com>
2021-03-15 14:02:46 -0400

    offline functions

---
+++ public/js/idb.js
@@ -0,0 +1,86 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    // save reference to database
+    const db = event.target.result;
+    
+    db.createObjectStore('new_trans', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    
+    db = event.target.result;
+
+    
+    if (navigator.onLine) {
+        uploadTrans();
+    }
+};
+
+request.onerror = function(event) {
+ 
+    console.log(event.target.errorCode);
+};
+
+
+const saveRecord = record => {
+    
+    const transaction = db.transaction(['new_trans'], 'readwrite');
+
+   
+    const transObjectStore = transaction.objectStore('new_trans');
+
+  
+    transObjectStore.add(record);
+};
+
+const uploadTrans = () => {
+    
+    const transaction = db.transaction(['new_trans'], 'readwrite');
+
+   
+    const transObjectStore = transaction.objectStore('new_trans');
+
+  
+    const getAll = transObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+       
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST', 
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type' : 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    // open transaction
+                    const transaction = db.transaction(['new_trans'], 'readwrite');
+
+                   
+                    const transObjectStore = transaction.objectStore('new_trans');
+
+                    // clear 
+                    transObjectStore.clear();
+
+                    alert('All saved transactions have been submitted!');
+                    location.reload();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        };
+    };
+};
+
+ 
+window.addEventListener('online', uploadTrans);
