6c4892be007ddeb6f597a1f26925ec5feef1758b
jconeff <jessicaconeff@gmail.com>
2021-03-15 15:59:53 -0400

    server

--- server.js
+++ server.js
@@ -16,10 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-  useFindAndModify: false,
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
 // routes
